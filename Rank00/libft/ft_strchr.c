2db0646ab7a875ff4cf1135eaa26f1793c7e139b
LuirR91 <141123658+LuirR91@users.noreply.github.com>
2024-05-08 15:31:57 +0100

    Add files via upload

---
+++ Rank00/libft/ft_strchr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luiribei <luiribei@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/12 14:40:40 by luiribei          #+#    #+#             */
+/*   Updated: 2024/05/06 10:18:10 by luiribei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strchr(const char *str, int c)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+			return ((char *)&str[i]);
+		i++;
+	}
+	if (c == '\0')
+		return ((char *)&str[i]);
+	return (0);
+}
+
+/*
+int	main()
+{
+	printf("%s\n", ft_strchr("carrot", 'a'));
+	return (0);
+}
+*/
